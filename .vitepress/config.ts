f1adc2711c7741cac09f8657ed44cee0d7fb15d0
IronRookieCoder <kdemo648@gmail.com>
2026-01-12 19:44:22 +0800

    ``` feat(config): add favicon to site configuration
    - Add favicon link to the head section of the site
    - Set favicon path to /logo.ico for the blog
    ```

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   lang: 'zh-CN',
   title: 'IronRookieCoder',
   description: 'The blog for IronRookieCoder',
+  head: [['link', { rel: 'icon', href: '/logo.ico' }]],
   cleanUrls: true,
   buildEnd: genFeed
 })
